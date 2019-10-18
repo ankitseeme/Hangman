21ba3d3103095e42c2764612695282d359a036f6
ankitseeme <ankitseeme@gmail.com>
2019-10-18 21:57:48 +0530

    Changing text messages color and some variable names

--- hangman.py
+++ hangman.py
@@ -17,35 +17,35 @@ def wait():
 
 def instructions():
     """Print Initial Instructions for the user"""
-    print("Let's go over the rules first!! Press Enter to continue...")
+    print_input_text("Let's go over the rules first!! Press Enter to continue...", " : ")
     wait()
     clear_screen()
 
-    print("You'll be given a name... [Press Enter]")
+    print_input_text("You'll be given a word or phrase... [Press Enter]", " : ")
     wait()
     clear_screen()
 
-    print("You have to guess the name by guessing the "
-          "characters in the name, one by one... [Press Enter]")
+    print_input_text("You have to guess the word by guessing the "
+                     "characters in the name, one by one... [Press Enter]", " : ")
     wait()
     clear_screen()
 
-    print("Based on the length and complexity of the name, "
-          "you'll be given a finite number of chances [Press Enter]")
+    print_input_text("Based on the length and complexity of the word, "
+                     "you'll be given a finite number of chances [Press Enter]", " : ")
     wait()
     clear_screen()
 
-    print("For every character you guess incorrectly, "
-          "you'll miss a chance [Press Enter]")
+    print_input_text("For every character you guess incorrectly, "
+                     "you'll miss a chance [Press Enter]", " : ")
     wait()
     clear_screen()
 
-    print("You lose when your chances get down to ZERO [Press Enter]")
+    print_input_text("You lose when your chances get down to ZERO [Press Enter]", " : ")
     wait()
     clear_screen()
 
-    print("You win if you guess the name with some chances "
-          "left on the board [Press Enter]")
+    print_input_text("You win if you guess the name with some chances "
+                     "left on the board [Press Enter]", " : ")
     wait()
     clear_screen()
 
@@ -86,54 +86,64 @@ def get_chances(name):
     return chances
 
 
-def guess(ch, chances):
+def guess(char_choice, chances):
     """Check if the given character is in the name,if chances permits.
     Returns chances_left, return_msg, guess_flag
     guess_flag is one of:
     e: if chance was unsuccessful
     s: if chance was successful
     """
-    ch = ch.upper()
+    char_choice = char_choice.upper()
     guess_flag = 'e'
-    if ch == '':
+
+    if char_choice == '':
         return_msg = "Please give an Input"
-    elif not ch.isalpha():
+
+    elif not char_choice.isalpha():
         return_msg = "You haven't given an english alphabet"
-    elif len(ch) != 1:
+
+    elif len(char_choice) != 1:
         return_msg = "Length of the character should be 1"
-    elif ch in GUESSED:
-        return_msg = "You have already used {}".format(ch.upper())
-    elif ch in NAME:
-        return_msg = Fore.GREEN + Style.BRIGHT + "Congrats! {} is in the name".format(ch.upper()) + Style.RESET_ALL
-        ind = [i for i, ltr in enumerate(NAME) if ltr == ch]
+
+    elif char_choice in GUESSED:
+        return_msg = "You have already used {}".format(char_choice.upper())
+
+    elif char_choice in NAME:
+        return_msg = Fore.GREEN + Style.BRIGHT + "Congrats! {} is in the name" \
+            .format(char_choice.upper()) + Style.RESET_ALL
+        ind = [i for i, ltr in enumerate(NAME) if ltr == char_choice]
         for i in ind:
-            CODED[i] = ch
-        GUESSED.append(ch)
+            CODED[i] = char_choice
+        GUESSED.append(char_choice)
         guess_flag = 's'
+
     else:
-        return_msg = "{} is not in the Name".format(ch.upper()) + Style.RESET_ALL
-        GUESSED.append(ch)
+        return_msg = "{} is not in the Name".format(char_choice.upper()) + Style.RESET_ALL
+        GUESSED.append(char_choice)
         chances -= 1
+
     return chances, return_msg, guess_flag
 
 
-def stats():
+def print_stats():
     """Print Stats for user, if he wants to see it,
     or compulsorily at the end of the game"""
-    print("-" * 15)
-    print("You have played {} times".format(PLAYED))
-    print("You have won " + Fore.GREEN + str(WON) + Style.RESET_ALL + " times")
-    print("You have lost " + Fore.RED + str(LOSS) + Style.RESET_ALL + " times")
+    clear_screen()
+    print_main_info("You have played {} times".format(PLAYED))
+    print("You have won " + Fore.LIGHTGREEN_EX + Style.BRIGHT +
+          str(WON) + Style.RESET_ALL + " times")
+    print("You have lost " + Fore.LIGHTRED_EX + Style.BRIGHT +
+          str(LOSS) + Style.RESET_ALL + " times")
     print_new_line()
 
 
 def replay():
     """Ask user if he wants to play another round"""
-    print("Press T and ENTER for your tally for this game or Y to play again")
+    print_input_text("Press ENTER to PLAY AGAIN, T for TALLY, and N to see TALLY and EXIT", " : ")
     inp = input().upper()
     if inp == 'T':
-        stats()
-        print("Press Enter to Continue")
+        print_stats()
+        print_input_text("Press ENTER to Continue", " : ")
         wait()
     return inp
 
@@ -148,87 +158,88 @@ def print_new_line():
     print("")
 
 
-def print_main_info(msg, end="\n"):
+def print_main_info(msg, line_sep="\n"):
     """Print INFO text"""
-    print(Fore.LIGHTCYAN_EX + Style.BRIGHT + msg + Style.RESET_ALL)
+    print(Fore.LIGHTCYAN_EX + Style.BRIGHT + msg + Style.RESET_ALL, end=line_sep)
 
 
-def print_warning_info(msg, end="\n"):
+def print_warning_info(msg, line_sep="\n"):
     """Print WARNING text"""
-    print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + msg + Style.RESET_ALL)
+    print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + msg + Style.RESET_ALL, end=line_sep)
+
+
+def print_input_text(msg, line_sep="\n"):
+    """Print text message for INPUT"""
+    print(Fore.YELLOW + Style.BRIGHT + msg + Style.RESET_ALL, end=line_sep)
 
 
 if __name__ == '__main__':
 
-    GENRE_FILE = "Genres.txt"
-
     clear_screen()
+
+    GENRE_FILE = "Genres.txt"
+    MASK = "_"
+    PLAYED = WON = LOSS = 0
+    CONT = 'Y'
 
     print_main_info("Welcome!! To a simple and exciting game of Hangman")
     print_new_line()
 
-    print_main_info("Do you want to go over the instructions or just dive"
-                    "in ?? [Y for instructions, N for passing it ]")
+    print_input_text("Do you want to go over the instructions or just dive"
+                     "in ?? [Y for instructions, N for passing it]", " : ")
 
     if input().upper() == 'Y':
         clear_screen()
         instructions()
 
     clear_screen()
-
-    print_main_info("Let's BEGIN.... [Press Enter]")
+    print_main_info("Let's BEGIN.... [Press Enter]", " : ")
 
     wait()
     clear_screen()
-
-    MASK = "_"
-
-    PLAYED = WON = LOSS = 0
-
-    CONT = 'Y'
 
     while CONT in ('Y', 'T', ''):
         PLAYED += 1
         clear_screen()
 
         GENRE = get_genre(GENRE_FILE)
-        F_NAME = GENRE + ".txt"
+        FILE_NAME = GENRE + ".txt"
 
-        NAME = get_name(F_NAME)
+        NAME = get_name(FILE_NAME)
 
         CODED = get_coded_name(NAME, MASK)
+
         PREV_CHANCES = CHANCES = get_chances(NAME)
 
         GUESSED = []
-
-        guessed_msg = ''
-        guessed_flag = 's'
+        GUESSED_MSG = ''
+        GUESSED_FLAG = 's'
 
         while CHANCES > 0:
 
             clear_screen()
 
-            print("The Genre is " + Fore.LIGHTGREEN_EX + Style.BRIGHT +
+            print("The Genre is " + Fore.LIGHTBLUE_EX + Style.BRIGHT +
                   GENRE.upper() + Style.RESET_ALL)
 
             print(*CODED)
 
             if CHANCES <= 2:
-                print(Fore.RED + Style.BRIGHT + "CHANCES = "
-                                                "{}".format(CHANCES) + Style.RESET_ALL)
+                print(Fore.RED + Style.BRIGHT + "CHANCES = {}"
+                      .format(CHANCES) + Style.RESET_ALL)
 
             else:
                 print("CHANCES = {}".format(CHANCES))
 
-            if guessed_msg and guessed_flag == 's':
-                print(guessed_msg)
-            elif guessed_msg:
-                print_warning_info(guessed_msg)
+            if GUESSED_MSG and GUESSED_FLAG == 's':
+                print(GUESSED_MSG)
+            elif GUESSED_MSG:
+                print_warning_info(GUESSED_MSG)
 
             PREV_CHANCES = CHANCES
 
             CHAR = input("Guess a character: ")
-            CHANCES, guessed_msg, guessed_flag = guess(CHAR, CHANCES)
+            CHANCES, GUESSED_MSG, GUESSED_FLAG = guess(CHAR, CHANCES)
 
             if ''.join(CODED) == NAME:
                 print(*CODED)
@@ -237,21 +248,21 @@ if __name__ == '__main__':
         if CHANCES == 0:
             clear_screen()
             LOSS += 1
-            print(Fore.RED + Style.BRIGHT + "You LOSE :( " + Style.RESET_ALL)
-            print(Fore.GREEN + Style.BRIGHT + "The Correct Name was "
-                                              ":: {}".format(NAME) + Style.RESET_ALL)
-            print_new_line()
+            print(Fore.RED + Style.BRIGHT + "You LOSE :(" + Style.RESET_ALL)
 
         else:
             clear_screen()
             WON += 1
-            print(Fore.GREEN + Style.BRIGHT + "You WIN" + Style.RESET_ALL)
-            print_new_line()
+            print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "You WIN :)" + Style.RESET_ALL)
+
+        print_main_info("The Correct Answer was", " :: ")
+        print_warning_info(NAME)
+        print_new_line()
 
         CONT = replay()
 
     print_main_info("Thanks for playing: ")
-    stats()
+    print_stats()
 
     print_main_info(Fore.WHITE + Style.BRIGHT + "Press Enter to EXIT" + Style.RESET_ALL)
     wait()
